37aef30df637f59ee76cd10d2944ebb8b9f509db
yangjun <yangjun@dinktalk.com>
2022-01-20 17:52:11 +0800

    update

--- pythonCode/No1-50/no28.py
+++ pythonCode/No1-50/no28.py
@@ -7,12 +7,14 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        # O(n)时间复杂度，
         if haystack == needle:
             return 0
 
         if not needle and haystack:
             return 0
 
+        # 获得needle第一个元素在haystack中出现的索引位置
         needleStartStrIndexList = []
         for i in range(len(haystack)):
             if haystack[i] == needle[0]:
@@ -21,9 +23,12 @@ class Solution:
         if not needleStartStrIndexList:
             return -1
 
+        # 判断haystack索引位置开始到needle长度的str是否和needle相同
         for i in needleStartStrIndexList:
             if haystack[i: i + len(needle)] == needle:
                 return i
+
+        return -1
 
 
 if __name__ == "__main__":
